a866cb51ad33e09ef92292ae9d24da78ff821920
soff22 <sofy@bitex.bg>
2015-04-09 22:02:02 +0300

    endless game

--- rock_paper.rb
+++ rock_paper.rb
@@ -1,20 +1,27 @@
 options = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
 question = "What do you choose?"
-puts question
-user = gets.chomp
-puts "Your choice is " + user
-choices = ["rock", "scissors", "paper"]
-computer = choices.sample
-if not choices.include? user
-	puts "Read the game rules"
-else
-	puts "What does the computer choose?"
-	puts computer
-	if user == computer
-		puts "It`s a tie"
-	elsif options[user] == computer
-		puts "You win"
-	else 
-		puts "Computer wins"
+while true
+	puts question
+	user = gets.chomp
+	puts "Your choice is " + user
+	choices = ["rock", "scissors", "paper"]
+	computer = choices.sample
+	finish = "quit"
+	if not choices.include? user
+		puts "Read the game rules"
+	else
+		puts "What does the computer choose?"
+		puts computer
+		if user == computer
+			puts "It`s a tie"
+		elsif options[user] == computer
+			puts "You win"
+		else 
+			puts "Computer wins"
+		#puts "End the game"
+		#break
+		end
 	end
 end
+
+
