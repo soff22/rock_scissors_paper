46bd4021d676f2b68dc1cd5e8129ee4cd1e08f33
soff22 <sofy@bitex.bg>
2015-04-17 22:08:26 +0300

    announce the winner

--- rock_paper.rb
+++ rock_paper.rb
@@ -1,6 +1,8 @@
 options = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
 question = "What do you choose? or Type \"quit\" to exit"
 number_of_games_played = 1
+user_wins = 0
+computer_wins = 0
 while number_of_games_played <= 6 do
 	print "#{number_of_games_played} "
 	break if number_of_games_played > 5
@@ -20,12 +22,23 @@ while number_of_games_played <= 6 do
 			puts "It`s a tie"
 		elsif options[user] == computer
 			puts "You win"
+			user_wins += 1
 		else 
 			puts "Computer wins"
+			computer_wins += 1
 		end
 	end
 end
 	puts "End of the Game"
+	puts "User result is #{user_wins}"
+	puts "Computer result is #{computer_wins}"
+	if user_wins > computer_wins
+		puts "User wins the Game"
+	elsif user_wins < computer_wins
+		puts "Computer wins the Game"
+	else 
+		puts "The result is equal"
+	end
 	
 
 	
